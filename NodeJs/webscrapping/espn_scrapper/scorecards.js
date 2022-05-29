5d05cf55de99567d6db93f94b8ebd702dd50135e
mohammadparvez921 <parrymd786@gmail.com>
2022-05-29 15:07:40 +0530

    project completed

--- NodeJs/webscrapping/espn_scrapper/scorecards.js
+++ NodeJs/webscrapping/espn_scrapper/scorecards.js
@@ -5,6 +5,9 @@ const path = require("path");
 const xlsx = require("xlsx");
 
 function getInfoFromScorecard(url) {
+//   console.log("from scorecards.js ",url);
+    // we have a url of a scorecard, we want to get html of that scorecard
+  // console.log("request received "+count);
   request(url, cb);
 }
 
